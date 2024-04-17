c447a65d31e8eb1c2f1911b3a21835366dc05238
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-17 19:39:12 -0400

    feat: Add resume edit form section

---
+++ src/components/Form.jsx
@@ -0,0 +1,4 @@
+import "../styles/form.css";
+export default function Form() {
+  return <div className="edit-form"></div>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Preview from "./components/Preview.jsx";
+import Form from "./components/Form.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Preview />
+    <div className="resume-wrapper">
+      <Form />
+      <Preview />
+    </div>
   </React.StrictMode>
 );
