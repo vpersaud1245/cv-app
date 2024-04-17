5e637231ecd54795cbcc99ae329308fc3161ce84
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-17 19:46:25 -0400

    feat: Add personal information section in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,32 @@
 import "../styles/form.css";
+
+function PersonalInformation() {
+  return (
+    <form action="" className="edit-form__personal-information">
+      <h1 className="form__title">Personal Information</h1>
+      <input
+        className="personal-information__first-name"
+        type="text"
+        placeholder="First Name"
+      />
+      <input
+        className="personal-information__last-name"
+        type="text"
+        placeholder="Last Name"
+      />
+      <input
+        className="personal-information__professional-title"
+        type="text"
+        placeholder="Professional Title"
+      />
+    </form>
+  );
+}
+
 export default function Form() {
-  return <div className="edit-form"></div>;
+  return (
+    <div className="edit-form">
+      <PersonalInformation />
+    </div>
+  );
 }
